6e2981a8f19d22b66b1254c52928e46bb28a07df
yj <yanjun@yilu-tech.com>
2019-09-23 21:04:35 +0800

    update

--- src/Validator.php
+++ src/Validator.php
@@ -4,11 +4,29 @@ namespace YiluTech\Validation;
 
 use Illuminate\Validation\Validator as ValidatorContract;
 use Illuminate\Support\Facades\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Validator extends ValidatorContract
 {
+    public function isValidateRequest()
+    {
+        return Request::hasHeader('Xhr-Form-Validator') || Request::hasHeader('VALIDATOR-REQUEST');
+    }
+
     public function passes()
     {
-        return parent::passes() && !Request::hasHeader('Xhr-Form-Validator') && !Request::hasHeader('VALIDATOR-REQUEST');
+        return parent::passes() && !$this->isValidateRequest();
+    }
+
+    public function validate()
+    {
+        try {
+            return parent::validate();
+        } catch (\Exception $exception) {
+            if ($this->messages->isEmpty()) {
+                throw new HttpException(200, 'Form validate success.');
+            }
+            throw $exception;
+        }
     }
 }
